e08f60d3d5bdece4787d109456e5cf6a00f79250
GlobalScript <91936237+GlobalScript@users.noreply.github.com>
2022-02-20 14:53:39 +0200

    finish

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,27 @@
 import { useEffect } from "react";
 import { useDispatch} from "react-redux";
-import { hiddenComponent} from "../store/elementVisibilitySlice";
+import { hiddenComponent, hiddenSort} from "../store/elementVisibilitySlice";
 
 function About() {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(hiddenComponent(true));
+        dispatch(hiddenSort(true));
+
     },[]);
     return <> 
-                <div className="banner-field"><h1>React js</h1></div>
+                <div className="banner-field"><h1>Pet Project</h1></div>
                 <div className="about-container">
-                <h1> Pet Project</h1>
-
+                <ul>
+                    <li>React js</li>
+                    <li>Redux Toolkit</li>
+                    <li>React Router</li>
+                    <li>Sass</li>
+                    <li>Lodash</li>
+                    <li><a href="https://github.com/GlobalScript/react-screw-shop">Git Hub</a></li>
+                </ul>
                 </div>
+                
             </>
 }
 
